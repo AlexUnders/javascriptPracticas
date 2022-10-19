645139aaadd06c509eb6e95340d1bd1d732563e2
Antonio <tonnigbsm@gmail.com>
2022-10-19 11:09:24 -0500

    Avance de las tablas

---
+++ tablas.js
@@ -0,0 +1,20 @@
+function tabla(){
+    let table = parseInt(document.getElementById("table").value);
+    let type = document.getElementById("type").value;
+    let long = document.getElementById("long".value)
+
+    switch (key) {
+        case "+":
+            for (let i = 0; i < long; i++) {
+            pass
+                
+            }
+            break;
+        case "-":
+            for (let i = long; i>0; i--)
+                pass
+            break;
+        default:
+            break;
+    }
+}
