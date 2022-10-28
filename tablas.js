64e37c8bd5b8b6fa553a614efc80aa697f0b3296
Antonio <tonnigbsm@gmail.com>
2022-10-28 13:16:54 -0500

    Lo que se me olvidó darle commit en clase

--- tablas.js
+++ tablas.js
@@ -1,20 +1,25 @@
 function tabla(){
     let table = parseInt(document.getElementById("table").value);
     let type = document.getElementById("type").value;
-    let long = document.getElementById("long".value)
-
-    switch (key) {
+    let long = document.getElementById("long").value;
+    let imprimir = document.getElementById("imprimir");
+    imprimir.innerHTML = "";
+    let res = 0;
+    switch (type) {
         case "+":
-            for (let i = 0; i < long; i++) {
-            pass
-                
+            for (let i = 1; i <= long; i++) {
+                res = table*i;
+                imprimir.innerHTML += table + "x"+ i + "=" + res+ "<br>";
             }
             break;
         case "-":
-            for (let i = long; i>0; i--)
-                pass
+            for (let i = long; i>0; i--){
+                res = table*i;
+                imprimir.innerHTML += table + "x"+ i + "=" + res+ "<br>";
+            }
             break;
         default:
+            imprimir.innerHTML = "Valores Inválidos";
             break;
     }
 }
