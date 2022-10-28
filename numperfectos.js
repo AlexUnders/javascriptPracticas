95210779b709493a71d8baef818ab8cd2f3ac495
Antonio <tonnigbsm@gmail.com>
2022-10-28 16:31:23 -0500

    Números perfectos terminado

---
+++ numperfectos.js
@@ -0,0 +1,20 @@
+function perfectos(){
+    let i = 1;
+    let e =1;
+    let cant = parseInt(document.getElementById("cant").value);
+    let imprimir = document.getElementById("imprimir");
+    let divisores = 0;
+    while(e<=cant){
+        for(let n = 1; n<i;n++){
+            if(i % n == 0){
+                divisores += n;
+            }
+        }
+        if(divisores == i){
+            imprimir.innerHTML += i + "<br>";
+            e++
+        }
+        divisores=0
+      i++
+    }
+}
