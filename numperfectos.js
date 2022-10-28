1939b0c19d5d57c6bb33c4f6aff6414f7a8e3e8f
Antonio <tonnigbsm@gmail.com>
2022-10-28 17:54:59 -0500

    Corrección del botón, ahora limpia lo que hay en pantalla cada que se presiona

--- numperfectos.js
+++ numperfectos.js
@@ -3,6 +3,7 @@ function perfectos(){
     let e =1;
     let cant = parseInt(document.getElementById("cant").value);
     let imprimir = document.getElementById("imprimir");
+    imprimir.innerHTML = "";
     let divisores = 0;
     while(e<=cant){
         for(let n = 1; n<i;n++){
